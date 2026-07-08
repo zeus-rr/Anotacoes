d30cc330555bfba991271bcf0783aaa601d4fc7c
Zeus-crtl <argeiphonteargus@gmail.com>
2026-07-08 13:04:10 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nine-anotacoes-v1';
+const CACHE_NAME = 'nine-anotacoes-v2';
 const ASSETS = [
   './index.html',
   './ICON.gif',
@@ -6,7 +6,6 @@ const ASSETS = [
   'https://unpkg.com/dexie@3.2.4/dist/dexie.js'
 ];
 
-// Instala o Service Worker e guarda os arquivos essenciais no cache do celular
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -15,7 +14,6 @@ self.addEventListener('install', (e) => {
   );
 });
 
-// Executa o aplicativo usando o cache se estiver sem internet
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
